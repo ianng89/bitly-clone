a09bb542d1c80cf465ad74e845bcf8f61da902cb
ianng89 <me@ian.ng>
2015-12-22 17:27:08 +0800

    first commit

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,18 @@
+require 'byebug'
 get '/' do
+
+	@urls = Url.all
+
   erb :"static/index"
+end
+
+post '/urls' do
+	# byebug
+	@url = Url.create(long_url: params[:long_url])
+	redirect to '/'
+end
+
+get '/:short_url' do
+
+
 end
